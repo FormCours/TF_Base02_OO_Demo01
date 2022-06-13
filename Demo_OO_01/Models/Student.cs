81d8ffa61f5c96e66031577f312c2c496a8ebc47
forma1200 <forma1200@FORMA-VDI1200>
2022-06-13 14:08:11 +0200

    Ajoutez des fichiers projet.

---
+++ Demo_OO_01/Models/Student.cs
@@ -0,0 +1,51 @@
+﻿namespace Demo_OO_01.Models
+{
+    class Student
+    {
+
+        // Fields
+        private string _Firstname;
+        private string _Lastname;
+        private double _Result;
+
+        // Properties
+        public string Firstname
+        {
+            get { return _Firstname; }
+            set { _Firstname = value; }
+        }
+
+        public string Lastname
+        {
+            get 
+            { 
+                return _Lastname.ToUpper();
+            }
+            set { _Lastname = value; }
+        }
+
+        public string FullName
+        {
+            get
+            {
+                return Firstname + " " + Lastname;
+            }
+        }
+
+        public double Result
+        {
+            get { return _Result; }
+            set 
+            {
+                // Test si la valeur est erroné
+                if(value > 20 || value < 0)
+                {
+                    // Générer une erreur (quand on aura vu comment)
+                    return;
+                }
+
+                _Result = value;
+            }
+        }
+    }
+}
---
+++ Demo_OO_01/Program.cs
@@ -0,0 +1,24 @@
+﻿using Demo_OO_01.Models;
+using System;
+
+namespace Demo_OO_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student zaza = new Student();
+            zaza.Firstname = "Zaza";
+            zaza.Lastname = "Vanderquack";
+            zaza.Result = 19;
+
+
+            Student della = new Student();
+            della.Firstname = "Della";
+            della.Lastname = "Duck";
+            della.Result = 22;
+        }
+    }
+
+
+}
